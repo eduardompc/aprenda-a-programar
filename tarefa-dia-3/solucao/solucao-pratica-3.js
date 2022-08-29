cedc8895a919c8ada8b686df3d914ab0996ccf18
eduardo <EDUARDOMPC@EMPCSERVICOS.COM.BR>
2022-08-28 21:40:35 -0300

    tarefas-empc

--- tarefa-dia-3/solucao/solucao-pratica-3.js
+++ tarefa-dia-3/solucao/solucao-pratica-3.js
@@ -1,9 +1,9 @@
-const n = 16 // aí o const novamente! :)
+ue i = i + 2
+}const n = 16 // aí o const novamente! :)
 let i = 1
 
 while (i <= n) {
     console.log(i)
-    i += 2 // que é a mesma coisa que i = i + 2
-}
+    i += 2 // que é a mesma coisa q
 
 
